e4b98cd6aa16c9f35dce48e91cf76e0c05135811
Chidii <chidiebere.levi@gmail.com>
2020-02-21 18:23:25 +0100

    first initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace Tic_Tac_Toe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.button1.FlatAppearance.BorderSize = 3;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(90, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(74, 69);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.button2.FlatAppearance.BorderSize = 3;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(170, 35);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(74, 69);
+            this.button2.TabIndex = 0;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.White;
+            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.button3.FlatAppearance.BorderSize = 3;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(250, 35);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(74, 69);
+            this.button3.TabIndex = 0;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.White;
+            this.button4.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.button4.FlatAppearance.BorderSize = 3;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(90, 110);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(74, 69);
+            this.button4.TabIndex = 0;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.White;
+            this.button5.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.button5.FlatAppearance.BorderSize = 3;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Location = new System.Drawing.Point(170, 110);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(74, 69);
+            this.button5.TabIndex = 0;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.White;
+            this.button6.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.button6.FlatAppearance.BorderSize = 3;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Location = new System.Drawing.Point(250, 110);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(74, 69);
+            this.button6.TabIndex = 0;
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.White;
+            this.button7.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.button7.FlatAppearance.BorderSize = 3;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.Location = new System.Drawing.Point(90, 185);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(74, 69);
+            this.button7.TabIndex = 0;
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.White;
+            this.button8.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.button8.FlatAppearance.BorderSize = 3;
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button8.Location = new System.Drawing.Point(170, 185);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(74, 69);
+            this.button8.TabIndex = 0;
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.White;
+            this.button9.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.button9.FlatAppearance.BorderSize = 3;
+            this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button9.Location = new System.Drawing.Point(250, 185);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(74, 69);
+            this.button9.TabIndex = 0;
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(353, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Player 1 = O";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(353, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Player 2 = X";
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(490, 307);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Century Schoolbook", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tic_Tac_Toe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int count = 0; //to count the number of turns
+
+
+        void restartGame()
+        {
+            Application.Restart();
+        }
+
+        // || this OR logic is not on my keyboard
+
+            
+       /* public Boolean checkwin(string symbol)
+        {
+            if (((button1.Text == symbol) && (button2.Text == symbol) && (button3.Text == symbol)) ||
+                ((button4.Text == symbol) && (button5.Text == symbol) && (button6.Text == symbol)) ||
+                ((button7.Text == symbol) && (button8.Text == symbol) && (button9.Text == symbol)) ||
+                ((button1.Text == symbol) && (button5.Text == symbol) && (button9.Text == symbol)) ||
+                ((button3.Text == symbol) && (button5.Text == symbol) && (button7.Text == symbol)) ||
+                ((button2.Text == symbol) && (button5.Text == symbol) && (button8.Text == symbol)) ||
+                ((button1.Text == symbol) && (button4.Text == symbol) && (button7.Text == symbol)) ||
+                ((button3.Text == symbol) && (button8.Text == symbol) && (button9.Text == symbol)))
+                {
+                return true;
+            }
+            return false;
+
+        }*/
+             
+
+
+        void checkwinner(string symbol)
+        {
+            if (button1.Text == symbol && button2.Text == symbol && button3.Text == symbol)
+            {
+                MessageBox.Show(symbol + " Won the Game");
+                restartGame();
+            }
+            else
+              if (button4.Text == symbol && button5.Text == symbol && button6.Text == symbol)
+            {
+                MessageBox.Show(symbol + " Won the Game");
+                restartGame();
+            }
+            else
+              if (button7.Text == symbol && button8.Text == symbol && button9.Text == symbol)
+            {
+                MessageBox.Show(symbol + " Won the Game");
+                restartGame();
+            }
+
+            else
+              if (button1.Text == symbol && button5.Text == symbol && button9.Text == symbol)
+            {
+                MessageBox.Show(symbol + " Won the Game");
+                restartGame();
+
+            }
+
+            else
+              if (button3.Text == symbol && button5.Text == symbol && button7.Text == symbol)
+            {
+                MessageBox.Show(symbol + " Won the Game");
+                restartGame();
+
+            }
+
+            else
+              if (button2.Text == symbol && button5.Text == symbol && button8.Text == symbol)
+            {
+                MessageBox.Show(symbol + " Won the Game");
+                restartGame();
+
+            }
+
+            else
+              if (button1.Text == symbol && button4.Text == symbol && button7.Text == symbol)
+            {
+                MessageBox.Show(symbol + " Won the Game");
+                restartGame();
+
+            }
+
+            else
+              if (button3.Text == symbol && button8.Text == symbol && button9.Text == symbol)
+            {
+                MessageBox.Show(symbol + " Won the Game");
+                restartGame();
+
+            }
+
+        }
+
+        /**public Boolean checkDraw(object senderobj)
+        {
+            //string buttontxt = ((Button)senderobj).Text;
+            if(checkwin(" ") {
+                MessageBox.Show("game Draw!");
+                return true;
+            }
+            return false;
+        }**/
+
+        void fnsymbol(object senderobj)
+        {
+            string buttontxt = ((Button)senderobj).Text;
+            if(buttontxt == "")
+            {
+                if (count % 2 == 0)
+                {
+                    ((Button)senderobj).Text = "O";
+                    label1.BackColor= Color.White;
+                    label2.BackColor = Color.Green;
+                }
+                else
+                {
+                    ((Button)senderobj).Text = "X";
+                    label2.BackColor = Color.White;
+                    label1.BackColor = Color.Green;
+
+                }
+                count++; //it will increament count, which turns to next player
+                checkwinner("O");
+                checkwinner("X");
+            }
+            else
+            {
+                MessageBox.Show("Invalid click");
+                
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            fnsymbol(sender);
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            fnsymbol(sender);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            fnsymbol(sender);
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            fnsymbol(sender);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            fnsymbol(sender);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            fnsymbol(sender);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            fnsymbol(sender);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            fnsymbol(sender);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            fnsymbol(sender);
+        }
+    }
+}
